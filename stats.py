3ccf48c1ec4e4e8066b61e3827074f176cabecfb
Miran-Mirantee <killerm11805@hotmail.com>
2025-07-04 17:58:19 +0700

    implement report_chars, refactor code

--- stats.py
+++ stats.py
@@ -1,3 +1,8 @@
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
 def count_words(string):
     arr = string.split()
     return len(arr)
@@ -12,3 +17,22 @@ def count_chars(string):
             else:
                 chars_dict[char] = 1
     return chars_dict
+
+def sort_on(items):
+    return items[1]
+
+def report_chars(filepath: str):
+    book_text = get_book_text(filepath)
+    chars_list = list(count_chars(book_text).items())
+    sorted_chars_list = sorted(chars_list, key=sort_on, reverse=True)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {count_words(book_text)} total words")
+    print("--------- Character Count -------")
+    
+    for chars in sorted_chars_list:
+        print(f"{chars[0]}: {chars[1]}")
+    
+    print("============= END ===============")
+    return
